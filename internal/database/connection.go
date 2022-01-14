8757f438829b64fa78fb63936a3df55f91279d34
Marcos <markinho.vedder@gmail.com>
2022-01-14 13:45:00 -0300

    fix(repo): organize go files to run

---
+++ internal/database/connection.go
@@ -0,0 +1,10 @@
+package database
+
+func Connect() {
+
+	// _, err := gorm.Open(mysql.Open("root:root@/teste"), &gorm.Config{})
+
+	// if err != nil {
+	// 	panic("could not connect to the database")
+	// }
+}
