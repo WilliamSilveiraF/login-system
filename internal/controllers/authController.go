6b4e58af5c3ae402ebeff6296ebf1ce165bfdad1
William Silveira <williamsilveiraf@hotmail.com>
2022-01-15 17:26:34 -0300

    feat(login): token generator

--- internal/controllers/authController.go
+++ internal/controllers/authController.go
@@ -1,11 +1,18 @@
 package controllers
 
 import (
-		"login-system/internal/models"
-		"login-system/internal/database"
-	 "github.com/gofiber/fiber/v2"
-		"golang.org/x/crypto/bcrypt"
+	"login-system/internal/database"
+	"login-system/internal/models"
+	"strconv"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
 )
+
+
+const SecretKey = "secret"
 
 func Register(c *fiber.Ctx) error {
 		var data map[string]string
@@ -52,5 +59,20 @@ func Login(c *fiber.Ctx) error {
 					 "message": "incorrect password",
 				})
 		}
-		return c.JSON(user)
+
+		claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		  Issuer: strconv.Itoa(int(user.Id)),
+				ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+		})
+
+		token, err := claims.SignedString([]byte(SecretKey))
+
+		if err != nil {
+			 c.Status(fiber.StatusInternalServerError)
+				return c.JSON(fiber.Map{
+					 "message": "could not login",
+				})
+		}
+
+		return c.JSON(token)
 }
