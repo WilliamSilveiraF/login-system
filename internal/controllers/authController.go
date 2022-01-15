f24e3db2e10c4e04e2a8fd71122bdcfaf5ae2867
William Silveira <williamsilveiraf@hotmail.com>
2022-01-15 17:53:09 -0300

    feat(login): Authenticated User

--- internal/controllers/authController.go
+++ internal/controllers/authController.go
@@ -73,6 +73,40 @@ func Login(c *fiber.Ctx) error {
 					 "message": "could not login",
 				})
 		}
+		
+		cookie := fiber.Cookie{
+			 Name: "jwt",
+				Value: token,
+				Expires: time.Now().Add(time.Hour * 24),
+				HTTPOnly: true,
+		}
 
-		return c.JSON(token)
+		c.Cookie(&cookie)
+
+		return c.JSON(fiber.Map{
+				"message": "sucess",
+		})
+}
+
+func User(c *fiber.Ctx) error {
+	 cookie := c.Cookies("jwt")
+
+		token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+			return []byte(SecretKey), nil
+		})
+
+		if err != nil {
+			 c.Status(fiber.StatusUnauthorized)
+				return c.JSON(fiber.Map{
+					 "message": "unauthorized",
+				})
+		}
+
+		claims := token.Claims.(*jwt.StandardClaims)
+
+		var user models.User
+
+		database.DB.Where("id = ?", claims.Issuer).First(&user)
+
+		return c.JSON(user)
 }
